5032f0f833bce354740e1bb8ab58eba846b2eac9
P1NHE4D <aleks.michael.g2@gmail.com>
2023-03-17 17:04:05 +0100

    adjust docker files

--- norgpt3.sh
+++ norgpt3.sh
@@ -8,11 +8,12 @@ DISTRIBUTED_ARGS="--nproc_per_node 4 \
                   --master_addr localhost \
                   --master_port 6000"
 
-CHECKPOINT=/data/all/models/nor_gpt3/latest_checkpointed_iteration.txt
-VOCAB_FILE=/data/all/models/nor_gpt3/vocab.json
-MERGE_FILE=/data/all/models/nor_gpt3/merges.txt
-CUDA_HOME='/usr/local/cuda-11.4'
+CHECKPOINT=/models/nor_gpt3/latest_checkpointed_iteration.txt
+VOCAB_FILE=/models/nor_gpt3/vocab.json
+MERGE_FILE=/models/nor_gpt3/merges.txt
+# CUDA_HOME='/usr/local/cuda-12.1'
 pip install flask-restful
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 #--temperature: Sampling temperature.
 #--top_p: Top p sampling.
@@ -29,7 +30,7 @@ python -m torch.distributed.run $DISTRIBUTED_ARGS ./tools/run_text_generation_se
        --fp16  \
        --micro-batch-size 4  \
        --seq-length 2048  \
-#       --out-seq-length 1024  \
+       --out-seq-length 1024  \
        --temperature 1.0  \
        --vocab-file $VOCAB_FILE  \
        --merge-file $MERGE_FILE  \
